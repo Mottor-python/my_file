26f9172e13180d3d6638795360bf61d73fd681ba
Mottor-python <p.s.alex.rb@gmail.com>
2023-03-18 19:25:56 +0200

    my file

--- settingdays.py
+++ settingdays.py
@@ -1,37 +1,32 @@
 import datetime
 import itertools
 
-# Функція
+# Function
 def raspisanie(date_start, date_end, days_work, days_skip):
     startDate = datetime.datetime.strptime(date_start, '%Y-%m-%d').date()
     endDate = datetime.datetime.strptime(date_end, '%Y-%m-%d').date()
     alldays = endDate - startDate
-
     data = []
-    # Додаєм в список всі дні між вказаними датами
+    newData = []
+
+    # We add all the days between the specified dates to the list
     for y in range(alldays.days + 1):
         calendarDate = startDate + datetime.timedelta(days=y)
         data.append(calendarDate.isoformat())
 
-
-
-    newData = []
-
     while (len(data) > 0):
-
-        for num in  range(days_work):
+        for num in range(days_work):
             if (len(data) > 0):
-
                 item = data[:1:]
                 newData.append(str(item[0]))
                 data.pop(0)
-
-        for num in  range(days_skip):
+        for num in range(days_skip):
             if (len(data) > 0):
                 data.pop(0)
+
     print(newData)
 
-# Виклик функції
+# Function call
 raspisanie("2022-04-20", "2022-04-23", 1, 1)
 raspisanie("2022-04-25", "2022-06-26", 0, 4)
 raspisanie("2022-05-14", "2022-05-24", 1, 95)
